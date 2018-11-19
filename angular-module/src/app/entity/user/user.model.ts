a87a9fe4b187ebfb882ed771489c1c01c75cdd31
mprouve <prouve.maxim@hotmail.fr>
2018-11-19 20:05:44 +0100

    feat(init): delete laravel module

--- angular-module/src/app/entity/user/user.model.ts
+++ angular-module/src/app/entity/user/user.model.ts
@@ -1,5 +1,5 @@
-export class User {
-    email?: string;
-    firstname?: string;
-    lastname?: string;
+export interface User {
+    email: string;
+    firstname: string;
+    lastname: string;
 }
